3b979bc9ffd5efde8300adbaea25aa0a67543f9c
David M. Strickland <dstricks@gmail.com>
2013-10-16 15:43:13 -0400

    Javadoc and variable rename
    - Add Javadocs to all existing methods
    - Rename method arguments to better represent purpose

--- src/main/java/net/davidstrickland/Tesseract.java
+++ src/main/java/net/davidstrickland/Tesseract.java
@@ -13,29 +13,37 @@ import javax.imageio.ImageIO;
 import org.apache.commons.io.FileUtils;
 
 /**
- * Usage:tesseract imagename outputbase [-l lang] [-psm pagesegmode] [configfile...]
-pagesegmode values are:
-0 = Orientation and script detection (OSD) only.
-1 = Automatic page segmentation with OSD.
-2 = Automatic page segmentation, but no OSD, or OCR
-3 = Fully automatic page segmentation, but no OSD. (Default)
-4 = Assume a single column of text of variable sizes.
-5 = Assume a single uniform block of vertically aligned text.
-6 = Assume a single uniform block of text.
-7 = Treat the image as a single text line.
-8 = Treat the image as a single word.
-9 = Treat the image as a single word in a circle.
-10 = Treat the image as a single character.
--l lang and/or -psm pagesegmode must occur before anyconfigfile.
- * @author david
- *
+ * A simple Java wrapper for the Tesseract command line tool
+ * <br/><br/>
+ * *Important Note: This library requires a system variable called "tesseract" which is the location of the Tesseract tool (ex. -Dtesseract=/usr/bin/tesseract)
  */
 public class Tesseract {
-	public static String process(String imagename) throws IOException {
-		return process(imagename, null);
+	/**
+	 * OCR the image located at <code>imagePath</code> using the
+	 * English language pack.
+	 *
+	 * @param imagePath path to the image (ex. C:/Documents/image.jpg)
+	 * @return Any text that was extracted, or empty String if no text was extracted
+	 * @throws IOException
+	 * 				if either (1) a source file could not be found <code>imagePath</code>
+	 * 				or (2) the system doesn't have access to create temporary files.
+	 */
+	public static String process(String imagePath) throws IOException {
+		return process(imagePath, null);
 	}
 	
-	public static String process(String imagename, String language) throws IOException {
+	/**
+	 * OCR the image located at <code>imagePath</code> using the language pack
+	 * that corresponds to passed in <code>langCode</code>.
+	 * 
+	 * @param imagePath path to the image (ex. C:/Documents/image.jpg)
+	 * @param langCode Tesseract-specific language code to use for OCR (ex. eng)
+	 * @return Any text that was extracted, or empty String if no text was extracted
+	 * @throws IOException
+	 * 				if either (1) a source file could not be found <code>imagePath</code>
+	 * 				or (2) the system doesn't have access to create temporary files.
+	 */
+	public static String process(String imagePath, String langCode) throws IOException {
 		String tesseractPath = System.getProperty("tesseract");
 		
 		if(tesseractPath == null) {
@@ -46,11 +54,11 @@ public class Tesseract {
 		
 		List<String> args = new ArrayList<String>();
 		args.add(tesseractPath);
-		args.add(imagename);
+		args.add(imagePath);
 		args.add(id);
 
-		if(language != null) {
-			args.add("-l " + language);
+		if(langCode != null) {
+			args.add("-l " + langCode);
 		}
 
 		ProcessBuilder builder = new ProcessBuilder(args);
@@ -70,9 +78,16 @@ public class Tesseract {
 		return text;
 	}
 	
-	public static String process(InputStream image) throws IOException {
+	/**
+	 * OCR the <code>imageStream</code> using the English language pack.
+	 *
+	 * @param imageStream the image as an InputStream
+	 * @return Any text that was extracted, or empty String if no text was extracted
+	 * @throws IOException if the system doesn't have access to create temporary files
+	 */
+	public static String process(InputStream imageStream) throws IOException {
 		File tempFile = File.createTempFile("tesseract_", ".tmp");
-		FileUtils.copyInputStreamToFile(image, tempFile);
+		FileUtils.copyInputStreamToFile(imageStream, tempFile);
 		
 		String text = process(tempFile.getPath());
 		
@@ -81,11 +96,19 @@ public class Tesseract {
 		return text;
 	}
 	
-	public static String process(BufferedImage image, String type) throws IOException {
+	/**
+	 * OCR the <code>bufferedImage</code> using the English language pack.
+	 * 
+	 * @param bufferedImage the image as a BufferedImage
+	 * @param formatName name of the Image format-- ex. "jpg" (see {@link ImageIO.getReaderFormatNames();})
+	 * @return Any text that was extracted, or empty String if no text was extracted
+	 * @throws IOException if the system doesn't have access to create temporary files
+	 */
+	public static String process(BufferedImage bufferedImage, String formatName) throws IOException {
 		String text = "";
 
 		File temp = File.createTempFile("tesseract_", ".tmp");
-		ImageIO.write(image, type, temp);
+		ImageIO.write(bufferedImage, formatName, temp);
 		
 		text = process(temp.getAbsolutePath());
 		temp.delete();
